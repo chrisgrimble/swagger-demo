05093c96136e0343b8260b9ceebb1d24ffc2b069
chrisgrimble <chris.grimble@hmcts.net>
2021-02-17 14:20:45 +0000

    More annotation examples

--- src/main/java/com/example/swaggerdemo/Greeting.java
+++ src/main/java/com/example/swaggerdemo/Greeting.java
@@ -1,8 +1,12 @@
 package com.example.swaggerdemo;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class Greeting {
 
+    @ApiModelProperty(example = "123")
     private final long id;
+    @ApiModelProperty(example = "Hi Bob, this is a jaunty message")
     private final String content;
 
     public Greeting(long id, String content) {
--- src/main/java/com/example/swaggerdemo/GreetingController.java
+++ src/main/java/com/example/swaggerdemo/GreetingController.java
@@ -1,18 +1,28 @@
 package com.example.swaggerdemo;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
+@Api(description = "Greetings description text")
 public class GreetingController {
 
     private static final String template = "%s, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @ApiOperation(value = "Short description of the greeting endpoint",
+        notes = "A description of the greeting endpoint")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "An ok greeting"),
+        @ApiResponse(code = 403, message = "Some other error"),
+    })
     @PostMapping("/greeting")
     public Greeting greeting(@RequestBody GreetingRequest greetingRequest) {
         return new Greeting(counter.incrementAndGet(), String.format(template, greetingRequest.getMessage(), greetingRequest.getName()));
